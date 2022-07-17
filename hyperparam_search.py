67c466edf13d1da55030d1685ac4788450d5ef45
Daniel Rosmæl Skauge <2001leinad@gmail.com>
2022-07-17 11:54:06 +0200

    adds initial files

---
+++ hyperparam_search.py
@@ -0,0 +1,17 @@
+def hyperparam_search():
+    print("Starting hyperparam-optimization")
+
+    params = {'tau': hp.uniform('tau', 0.001, 0.01),
+              'critic_lr': hp.uniform('critic_lr', 0.0002, 0.02),
+              'actor_lr': hp.uniform('actor_lr', 0.0001, 0.01),
+              'gamma': hp.uniform('gamma', 0.9, 0.999),
+              'noise_std_dev': hp.uniform('noise_std_dev', 0.1, 0.4), }
+
+    best = fmin(loss_given_hyperparams, params,
+                algo=hyperopt.rand.suggest, max_evals=100)
+
+    print(best)
+    print(hyperopt.space_eval(params, best))
+
+
+train(None)
